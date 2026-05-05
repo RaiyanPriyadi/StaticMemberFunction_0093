c6853f5ac519bdf7b2981a105827422b2a68580b
RaiyanPriyadi <dwi.raiyan.ft25@mail.umy.ac.id>
2026-05-05 16:03:30 +0700

    void

--- staticmember.cpp
+++ staticmember.cpp
@@ -24,3 +24,12 @@ public:
 int Mahasiswa::nim = 10;
 
 //mengimplementasikan prosedur - prosedur di luar class
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+    cout << "ID : " << id << endl;
+    cout << "Nama : " << nama << endl;
+    cout << endl;
+}
